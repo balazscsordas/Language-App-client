72f787d37837b83a65f81bb205fb15f01e1c7afc
Csordas Balazs <csordasbalu96@gmail.com>
2023-02-09 19:30:52 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Layout from '../../src/components/Layout'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Balazs Csordas | Portfolio</title>
+        <meta name="description" content="Balazs Csordas | Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+      </Layout>
+    </>
+  )
+}
